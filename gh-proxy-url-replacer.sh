70688649f482f200d5563fe1c34f173fd5e31109
Reborn <ren_xiaoyao@outlook.com>
2025-03-20 17:05:43 +0800

    Update gh-proxy-url-replacer.sh

--- gh-proxy-url-replacer.sh
+++ gh-proxy-url-replacer.sh
@@ -5,5 +5,5 @@ ICON_PATH="$2"
 # GH_PROXY_HOST="$3"
 mkdir $(dirname $ICON_PATH) -p
 echo $(curl $ICON_SRC_URL | sed "s|https://raw.githubusercontent.com|https://ghfast.top/https://raw.githubusercontent.com|g") | sed "s|https:\\\/\\\/raw.githubusercontent.com|https:\\\/\\\/ghfast.top\\\/https:\\\/\\\/raw.githubusercontent.com|g" > $ICON_PATH
-FINAL_ICON_PATH="https://ghfast.top/https://raw.githubusercontent.com/Reborn/Emby-icon-collection/refs/heads/main/$ICON_PATH"
+FINAL_ICON_PATH="https://ghfast.top/https://raw.githubusercontent.com/RebornQ/Emby-icon-collection/refs/heads/main/$ICON_PATH"
 echo $FINAL_ICON_PATH
